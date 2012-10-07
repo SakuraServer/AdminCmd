66bd5fb5af373d69522705c1b5802848b79d13e2
syamn <admin@sakura-server.net>
2012-10-07 22:04:34 +0900

    Disable freeze player listener

--- src/main/java/be/Balor/Listeners/Commands/ACFrozenPlayerListener.java
+++ src/main/java/be/Balor/Listeners/Commands/ACFrozenPlayerListener.java
@@ -1,16 +1,16 @@
 /************************************************************************
- * This file is part of AdminCmd.									
- *																		
+ * This file is part of AdminCmd.
+ *
  * AdminCmd is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by	
- * the Free Software Foundation, either version 3 of the License, or		
- * (at your option) any later version.									
- *																		
- * AdminCmd is distributed in the hope that it will be useful,	
- * but WITHOUT ANY WARRANTY; without even the implied warranty of		
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the			
- * GNU General Public License for more details.							
- *																		
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AdminCmd is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
  * You should have received a copy of the GNU General Public License
  * along with AdminCmd.  If not, see <http://www.gnu.org/licenses/>.
  ************************************************************************/
@@ -29,10 +29,10 @@ import be.Balor.Tools.Type;
 
 /**
  * @author Balor (aka Antoine Aflalo)
- * 
+ *
  */
 public class ACFrozenPlayerListener implements Listener {
-	@EventHandler(ignoreCancelled = true, priority = EventPriority.LOW)
+	//@EventHandler(ignoreCancelled = true, priority = EventPriority.LOW)
 	public void onMove(final PlayerMoveEvent event) {
 		if (!ACPlayer.getPlayer(event.getPlayer()).hasPower(Type.FROZEN)) {
 			return;
@@ -41,21 +41,21 @@ public class ACFrozenPlayerListener implements Listener {
 
 	}
 
-	@EventHandler(ignoreCancelled = true, priority = EventPriority.LOW)
+	//@EventHandler(ignoreCancelled = true, priority = EventPriority.LOW)
 	public void onDrop(final PlayerDropItemEvent event) {
 		if (ACPlayer.getPlayer(event.getPlayer()).hasPower(Type.FROZEN)) {
 			event.setCancelled(true);
 		}
 	}
 
-	@EventHandler(ignoreCancelled = true, priority = EventPriority.LOW)
+	//@EventHandler(ignoreCancelled = true, priority = EventPriority.LOW)
 	public void onPickup(final PlayerPickupItemEvent event) {
 		if (ACPlayer.getPlayer(event.getPlayer()).hasPower(Type.FROZEN)) {
 			event.setCancelled(true);
 		}
 	}
 
-	@EventHandler(ignoreCancelled = true, priority = EventPriority.LOW)
+	//@EventHandler(ignoreCancelled = true, priority = EventPriority.LOW)
 	public void onInteract(final PlayerInteractEvent event) {
 		if (ACPlayer.getPlayer(event.getPlayer()).hasPower(Type.FROZEN)) {
 			event.setCancelled(true);
